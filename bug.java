416e8618ed2d0561d76f7b56d52d44ed2818de15
BugHunter-X <askzenai@gmail.com>
2024-12-29 21:42:05 +0100

    Add bug.java file

---
+++ bug.java
@@ -0,0 +1,9 @@
+public class MyClass {
+    public static void main(String[] args) {
+        int x = 0;
+        while (x++ < 10) {
+            System.out.println(x); //This will print 1 to 10
+        }
+        System.out.println("Loop finished");
+    }
+}
